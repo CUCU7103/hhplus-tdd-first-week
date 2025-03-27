3131d3278baf078ee9f0077aea8cc86a504e15e0
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-28 03:50:51 +0900

    [Fix] 포인트 조회 테스트 코드 수정
    포인트 조회 테스트 코드 수정

--- src/test/java/io/hhplus/tdd/point/PointServiceUnitTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceUnitTest.java
@@ -3,10 +3,11 @@ package io.hhplus.tdd.point;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -23,6 +24,9 @@ class PointServiceUnitTest {
 	@Mock
 	private UserPointTable userPointTable;
 
+	@Mock
+	private ValidatorImpl validator;
+
 	@InjectMocks
 	private PointService pointService;
 
@@ -32,6 +36,7 @@ class PointServiceUnitTest {
 		long id = 1L;
 		long point = 100L;
 		UserPoint userPoint = new UserPoint(id, point, System.currentTimeMillis());
+		doNothing().when(validator).invalidId(id);
 		given(userPointTable.selectById(id)).willReturn(userPoint); // stubbing 객체의 상태를 반환
 		// act 테스트 실행
 		UserPoint result = pointService.searchUserPoint(id);
@@ -41,24 +46,29 @@ class PointServiceUnitTest {
 		assertThat(userPoint.point()).isEqualTo(point);
 	}
 
-	@ValueSource(longs = {0L, -100L})
-	@ParameterizedTest
-	void 포인트_조회시_유저_아이디가_0보다_작다면_예외가_발생한다(long id) {
-		// arrange 테스트 조건
-		// act & assert
-		assertThatThrownBy(() -> pointService.searchUserPoint(id)).isInstanceOf(RuntimeException.class);
-		
-	}
-
 	@Test
-	void 포인트_조회시_유저의_포인트가_음수라면_예외가_발생한다() {
-		// arrange
+	void 포인트_충전_이용내역_조회시_유저_아이디가_양수라면_정상적으로_조회된다() {
+		//arrange
 		long id = 1L;
-		long point = -100L;
-		UserPoint userPoint = new UserPoint(id, point, System.currentTimeMillis());
-		given(userPointTable.selectById(id)).willReturn(userPoint);
-		// act & assert
-		assertThatException().isThrownBy(() -> pointService.searchUserPoint(id)).isInstanceOf(RuntimeException.class);
+		long firstAmount = 100L;
+		long secondAmount = 100L;
+		PointHistory firstHistory = new PointHistory(1, id, firstAmount, TransactionType.CHARGE,
+			System.currentTimeMillis());
+		PointHistory secondHistory = new PointHistory(2, id, secondAmount, TransactionType.CHARGE,
+			System.currentTimeMillis());
+		doNothing().when(validator).invalidId(id);
+		List<PointHistory> histories = Arrays.asList(firstHistory, secondHistory);
+		given(pointHistoryTable.selectAllByUserId(id)).willReturn(histories);
+
+		// act
+		List<PointHistory> resultList = pointService.searchPointHistory(id);
+
+		// assert 검증
+		assertThat(resultList).hasSize(2);
+		assertThat(resultList.get(0).userId()).isEqualTo(id);
+		assertThat(resultList.get(1).userId()).isEqualTo(id);
+		assertThat(resultList.get(0).amount()).isEqualTo(firstAmount);
+		assertThat(resultList.get(1).amount()).isEqualTo(secondAmount);
 	}
 
 }
