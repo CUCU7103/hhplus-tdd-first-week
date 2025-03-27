bbb992388c832ae6b2fc2f809e6038505b9e8401
Joon Gyu <qkrwnsrb224@gmail.com>
2025-03-27 23:04:10 +0900

    [Feat] 포인트 충전, 사용 기능에 대한 동시성 제어 기능 추가
    ReentrantLock 을 사용하여 동시성 제어기능을 추가
    
    # Conflicts:
    #	src/main/java/io/hhplus/tdd/point/PointService.java

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,11 +1,14 @@
 package io.hhplus.tdd.point;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.springframework.stereotype.Service;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.lock.UserLockManager;
+import io.hhplus.tdd.validator.ValidatorImpl;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -15,6 +18,7 @@ public class PointService {
 	private final PointHistoryTable pointHistoryTable;
 	private final UserPointTable userPointTable;
 	private final ValidatorImpl validator;
+	private final UserLockManager userLockManager;
 
 	public UserPoint searchUserPoint(long id) {
 		validator.invalidId(id);
@@ -31,33 +35,52 @@ public class PointService {
 		return histories;
 	}
 
+	/**
+	 * 	동시성 제어를 하려면?<p>
+	 * 	동일한 사용자에 대해서 여러 스레드에서 충전 작업이 이루어질 경우 이 동시 접근에 대한 제어가 필요하다. <p>
+	 * 	각 사용자에 대한 개별적인 ReetrantLock을 사용하면 동일한 사용자에 대해서 여러 스레드에서 충전 작업이 이루어질 경우 동시 접근에 대한 제어가 가능하다. <p>
+	 * 	각기 다른 사용자에 대한 충전작업이 동시에 이루어질 경우 각 사용자에 대해 개별적 ReetrantLock을 사용하기에 병렬적으로 작업이 이루어진다.
+	 */
+
 	public UserPoint charge(long id, long amount) {
 		// 입력받은 값들을 검증하는 로직
 		validator.invalidId(id);
 		validator.invalidAmount(amount);
-		// 유저 정보를 가져오고 이를 검증하고 새로운 객체를 반환하는 역할을 밖으로 뺀다.
-		UserPoint userPoint = userPointTable.selectById(id);
-		UserPoint afterChargedUserPoint = userPoint.charge(amount);
-		// 유저 포인트 저장하기
-		UserPoint resultUserPoint = userPointTable.insertOrUpdate(afterChargedUserPoint.id(),
-			afterChargedUserPoint.point());
-		// 히스토리에 저장하는 기능
-		pointHistoryTable.insert(resultUserPoint.id(), resultUserPoint.point(), TransactionType.CHARGE,
-			System.currentTimeMillis());
-		return resultUserPoint;
+		ReentrantLock userLock = userLockManager.getUserLock(id);
+		userLock.lock();
+		try {
+			UserPoint userPoint = userPointTable.selectById(id);
+			// 유저 정보를 가져오고 이를 검증하고 새로운 객체를 반환하는 역할을 밖으로 뺀다.
+			UserPoint afterChargedUserPoint = userPoint.charge(amount);
+			// 유저 포인트 저장하기
+			UserPoint resultUserPoint = userPointTable.insertOrUpdate(userPoint.id(), amount);
+			// 히스토리에 저장하는 기능
+			pointHistoryTable.insert(resultUserPoint.id(), resultUserPoint.point(), TransactionType.CHARGE,
+				System.currentTimeMillis());
+			return afterChargedUserPoint;
+		} finally {
+			userLock.unlock();
+		}
 	}
 
 	public UserPoint use(long id, long amount) {
 		validator.invalidId(id);
 		validator.invalidAmount(amount);
 		// UserPoint 객체를 가져온다.
-		UserPoint userPoint = userPointTable.selectById(id);
-		// UserPoint에서 처리
-		UserPoint afterUsedUserPoint = userPoint.use(amount);
-		// 유저 포인트 저장하기
-		UserPoint resultUserPoint = userPointTable.insertOrUpdate(afterUsedUserPoint.id(), afterUsedUserPoint.point());
-		pointHistoryTable.insert(resultUserPoint.id(), resultUserPoint.point(), TransactionType.CHARGE,
-			System.currentTimeMillis());
-		return resultUserPoint;
+		ReentrantLock userLock = userLockManager.getUserLock(id);
+		userLock.lock();
+		try {
+			UserPoint userPoint = userPointTable.selectById(id);
+			// UserPoint에서 처리
+			UserPoint afterUsedUserPoint = userPoint.use(amount);
+			// 유저 포인트 저장하기
+			UserPoint resultUserPoint = userPointTable.insertOrUpdate(afterUsedUserPoint.id(),
+				afterUsedUserPoint.point());
+			pointHistoryTable.insert(resultUserPoint.id(), resultUserPoint.point(), TransactionType.USE,
+				System.currentTimeMillis());
+			return afterUsedUserPoint;
+		} finally {
+			userLock.unlock();
+		}
 	}
 }
